59cfc43fbde65ce301fd6574086772fb72954388
tmalcher <tmalcher@positivo.com.br>
2022-11-02 19:18:32 -0300

    project created to use the OTG connection and through it send files to the connected device

---
+++ app/src/main/java/com/malcher/usbhostmaster/MainActivity.java
@@ -0,0 +1,24 @@
+package com.malcher.usbhostmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+
+    }
+}
+
+
