b9864ac06cd42b85ca25f529835c57fe9bcbc306
tmalcher <tmalcher@positivo.com.br>
2022-11-05 20:26:52 -0300

    -changed to check if exist device connected.

--- app/src/main/java/com/malcher/usbhostmaster/utils/UsbManagerUtilities.java
+++ app/src/main/java/com/malcher/usbhostmaster/utils/UsbManagerUtilities.java
@@ -1,7 +1,12 @@
 package com.malcher.usbhostmaster.utils;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.widget.Toast;
 
@@ -11,34 +16,29 @@ import java.util.Iterator;
 
 public class UsbManagerUtilities {
 
+    static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
     private static String usbDeviceList = null;
     private static String realDeviceName = null;
+    private UsbDeviceConnection usbDeviceConnection;
+    private byte[] readBytes = new byte[64];
+    PendingIntent permissionIntent;
+
 
     public static boolean checkIfExistDeviceConnected(Context context, UsbManager usbManager) {
 
-        boolean existOrNot = false;
         try {
-            UsbDevice device;
             HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
-            Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
-
-            while(deviceIterator.hasNext()) {
-                device = deviceIterator.next();
-                usbDeviceList = device.getDeviceName();
+            if(deviceList.size() == 0) {
+                Toast.makeText(context, "Device not Connected, check cable or device and try again...", Toast.LENGTH_SHORT).show();
+                return false;
             }
 
-            if(usbDeviceList != null) {
-                existOrNot = true;
-            }
+            return true;
         }catch (Exception e) {
             Toast.makeText(context, "ERROR: not possible get UsbDevice", Toast.LENGTH_SHORT).show();
         }
 
-        if(!existOrNot) {
-            Toast.makeText(context, "Device not Connected, check cable or device and try again...", Toast.LENGTH_SHORT).show();
-        }
-
-        return existOrNot;
+        return false;
     }
 
     public static String getDeviceName(UsbManager usbManager) {
@@ -57,4 +57,17 @@ public class UsbManagerUtilities {
 
         return realDeviceName;
     }
+
+    public void usbConnection(Context context, UsbManager usbManager) {
+        if(checkIfExistDeviceConnected(context, usbManager)) {
+
+        }
+    }
+
+    public static void checkPermission(Context context) {
+        PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter intentFilter = new IntentFilter(ACTION_USB_PERMISSION);
+       // context.registerReceiver(usbReceiver, intentFilter);
+    }
+
 }
